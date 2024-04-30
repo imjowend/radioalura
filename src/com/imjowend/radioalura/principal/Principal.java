564075738a55a1d1d31c49e7c4b34765609b62f1
imjowend <joaquinvcestudio@gmail.com>
2024-04-30 13:28:57 -0500

    Main

---
+++ src/com/imjowend/radioalura/principal/Principal.java
@@ -0,0 +1,7 @@
+package com.imjowend.radioalura.Principal;
+import com.imjowend.radioalura.modelos.Cancion;
+public class Principal {
+    public static void main(String[] args) {
+        Cancion miCancion = new Cancion();
+    }
+}
