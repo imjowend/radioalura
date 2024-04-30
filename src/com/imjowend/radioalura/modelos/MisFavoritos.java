ecfb0218f3592306c6e871b7b2bcc35116054445
imjowend <joaquinvcestudio@gmail.com>
2024-04-30 14:41:27 -0500

    Mis favs

---
+++ src/com/imjowend/radioalura/modelos/MisFavoritos.java
@@ -0,0 +1,10 @@
+package com.imjowend.radioalura.modelos;
+public class MisFavoritos {
+    public void adicione(Audio audio){
+        if(audio.getClasificacion() >= 8){
+            System.out.println(audio.getTitulo() + "Es uno de los favoritos del momento");
+        } else {
+            System.out.println(audio.getTitulo() + "También es uno de los favoritos");
+        }
+    }
+}
--- src/com/imjowend/radioalura/principal/Principal.java
+++ src/com/imjowend/radioalura/principal/Principal.java
@@ -1,7 +1,36 @@
 package com.imjowend.radioalura.Principal;
 import com.imjowend.radioalura.modelos.Cancion;
+import com.imjowend.radioalura.modelos.Podcast;
+import com.imjowend.radioalura.modelos.MisFavoritos;
 public class Principal {
     public static void main(String[] args) {
         Cancion miCancion = new Cancion();
+        miCancion.setTitulo("Forever");
+        miCancion.setCantante("Kiss");
+
+        Podcast miPodcast = new Podcast();
+        miPodcast.setPresentador("Gabriela Aguiar");
+        miPodcast.setTitulo("Cafe.Tech");
+        //cancion
+        for (int i = 0; i < 100; i++) {
+            miCancion.meGusta();
+        }
+        for (int i = 0; i < 2000; i++) {
+            miCancion.reproduce();
+        }
+        // podcast
+        for (int i = 0; i < 100; i++) {
+            miPodcast.meGusta();
+        }
+        for (int i = 0; i < 8000; i++) {
+            miPodcast.reproduce();
+        }
+
+        System.out.println("Total reproducciones: " + miCancion.getTotalDeReproducciones());
+        System.out.println("Total de Me Gusta: " + miCancion.getTotalDeMeGusta());
+
+        MisFavoritos favoritos = new MisFavoritos();
+        favoritos.adicione(miPodcast);
+        favoritos.adicione(miCancion);
     }
 }
